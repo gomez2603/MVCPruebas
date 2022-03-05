fdf17a9add6c47c2af2427cc3fe332b5cd0a4419
gomez2603 <jgomez@grupocyh.com>
2022-03-05 13:18:42 -0600

    Productos

---
+++ Areas/Admin/Controllers/MarcasController .cs
@@ -0,0 +1,86 @@
+﻿using CursoMVC.AccesoDatos.Repositorio.IRepositorio;
+using CursoMVC.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MarcasController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public MarcasController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Upsert(int? id)
+        {
+            Marca marca = new Marca();
+            if (id == null)
+            {
+                return View(marca);
+            }
+          
+                marca = _unidadTrabajo.marca.Obtener(id.GetValueOrDefault());
+                if (marca == null) {
+                    return NotFound();
+                }
+                return View(marca);
+          
+        }
+
+
+        #region API
+        [HttpGet]
+        public IActionResult obtenerTodos() {
+            var todos = _unidadTrabajo.marca.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+        #endregion
+
+        [HttpDelete]
+        public IActionResult delete(int id)
+        {
+            var macarDb = _unidadTrabajo.marca.Obtener(id);
+             if(macarDb == null)
+            {
+                return Json(new { success = false, message = "Error al Borrar" });
+            }
+            _unidadTrabajo.marca.Remover(macarDb);
+            _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Borrado Exitosamente" });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Marca marca)
+        {
+            if (ModelState.IsValid)
+            {
+                if (marca.Id == 0)
+                {
+                    _unidadTrabajo.marca.Agregar(marca);
+
+                }
+                else
+                {
+                    _unidadTrabajo.marca.Actualizar(marca);
+                }
+                _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(marca);
+        }
+
+    }
+}
---
+++ Areas/Admin/Controllers/ProductosController.cs
@@ -0,0 +1,156 @@
+﻿using CursoMVC.AccesoDatos.Repositorio.IRepositorio;
+using CursoMVC.Modelos;
+using CursoMVC.Modelos.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace CursoMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductosController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        private readonly IWebHostEnvironment _hostenvironment;
+        public ProductosController(IUnidadTrabajo unidadTrabajo, IWebHostEnvironment hostenvironment)
+        {
+            _unidadTrabajo = unidadTrabajo;
+            _hostenvironment = hostenvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Upsert(int? id)
+        {
+            ProductoViewModel productoView = new ProductoViewModel
+            {
+                Producto = new Producto(),
+                CategoriaLista = _unidadTrabajo.categoria.ObtenerTodos().Select(c => new SelectListItem
+                {
+                    Text = c.Nombre,
+                    Value = c.Id.ToString()
+                }),
+                MarcaLista = _unidadTrabajo.marca.ObtenerTodos().Select(m => new SelectListItem
+                {
+                    Text = m.Nombre,
+                    Value = m.Id.ToString()
+                }),
+
+            };
+            if (id == null)
+            {
+                return View(productoView);
+            }
+
+            productoView.Producto= _unidadTrabajo.producto.Obtener(id.GetValueOrDefault());
+            if (productoView.Producto == null) {
+                return NotFound();
+            }
+            return View(productoView);
+
+        }
+    
+
+        #region API
+        [HttpGet]
+        public IActionResult obtenerTodos() {
+            var todos = _unidadTrabajo.producto.ObtenerTodos(incluirPropiedades:"Categoria,Marca");
+            return Json(new { data = todos });
+        }
+        #endregion
+
+        [HttpDelete]
+        public IActionResult delete(int id)
+        {
+            var productoDb = _unidadTrabajo.producto.Obtener(id);
+             if(productoDb == null)
+            {
+                return Json(new { success = false, message = "Error al Borrar" });
+            }
+            _unidadTrabajo.producto.Remover(productoDb);
+            _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Borrado Exitosamente" });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductoViewModel productoVM)
+        {
+            if (ModelState.IsValid)
+            {
+                //Cargar Imagenes
+                string webRootPath = _hostenvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    string filename = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"img\productos");
+                    var extension = Path.GetExtension(files[0].FileName);
+                    if (productoVM.Producto.ImagenURL != null) {
+
+                        //esto es para editar la imagen anterior}
+                        var imagenPath = Path.Combine(webRootPath, productoVM.Producto.ImagenURL.TrimStart('\\'));
+                        if (System.IO.File.Exists(imagenPath)) {
+                            System.IO.File.Delete(imagenPath);
+                        }
+                    }
+
+                    using (var filesStreams = new FileStream(Path.Combine(uploads, filename + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(filesStreams);
+                    }
+                    productoVM.Producto.ImagenURL = @"\img\producto\" + filename + extension;
+                }
+                else
+                {
+                    //si no cambia la imagen 
+                    if (productoVM.Producto.Id != 0) {
+                        Producto productoDb = _unidadTrabajo.producto.Obtener(productoVM.Producto.Id);
+                        productoVM.Producto.ImagenURL = productoDb.ImagenURL;
+                    } 
+                }
+
+
+
+
+
+                if (productoVM.Producto.Id == 0)
+                {
+                    _unidadTrabajo.producto.Agregar(productoVM.Producto);
+
+                }
+                else
+                {
+                    _unidadTrabajo.producto.Actualizar(productoVM.Producto);
+                }
+                _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                productoVM.CategoriaLista = _unidadTrabajo.categoria.ObtenerTodos().Select(c => new SelectListItem
+                {
+                    Text = c.Nombre,
+                    Value = c.Id.ToString()
+                });
+                productoVM.MarcaLista = _unidadTrabajo.marca.ObtenerTodos().Select(m => new SelectListItem
+                {
+                    Text = m.Nombre,
+                    Value = m.Id.ToString()
+                });
+                if (productoVM.Producto.Id != 0) {
+                    productoVM.Producto= _unidadTrabajo.producto.Obtener(productoVM.Producto.Id);
+                }
+
+            }
+            return View(productoVM);
+        }
+
+    }
+}
